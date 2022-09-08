04b3af9d543b4a09ead8cdf10c51a6adbd3e9bd7
Misbauddin Chowdhury Shahan <misbauddin1994@gmail.com>
2022-09-08 00:27:47 +0000

    Update 3 files

--- README.md
+++ README.md
@@ -14,13 +14,14 @@ yarn add graphql-apollo-client
 
 ### Example
 
+
 ```js
-import configureClient from 'graphql-apollo-client';
+import { configureClient } from 'graphql-apollo-client';
 
 const client = configureClient({
-  httpEndpoint, 
-  wsEndpoint, 
-  bearerToken
+  httpEndpoint: '<HTTP_GRAPHQL_URL>', 
+  wsEndpoint: '<WS_GRAPHQL_URL>', 
+  bearerToken: 'Bearer <TOKEN>'
 })
 `
 ```
@@ -29,32 +30,37 @@ The above code is to configure client. Now it's time to use this client
 Note: To define query you need to install graphql-tag to use `gql`
 
 ```js
-import { gql } from 'graphql-tag';
+import { gql } from 'graphql-apollo-client';
+
+
+const query = gql`
+  query Books {
+    books {
+      title
+      author
+    }
+  }
+`
+
+const subscription = gql`
+  subscription {
+    realtimeMeasurement(deviceId: "pM8SEyRQ") {
+      activePowerImport
+      time
+    }
+  }
+`
 
 client
   .query({
-    query: gql`
-      query Books {
-        books {
-          title
-          author
-        }
-      }
-    `,
+    query: query,
     variables: {},
   })
   .then(console.log)
 
 client
  .subscribe({
-   query: gql`
-     subscription {
-       realtimeMeasurement(deviceId: "pM8SEyRQ") {
-         activePowerImport
-         time
-       }
-     }
-   `,
+   query: subscription,
    variables: {}
  })
  .subscribe({
--- index.js
+++ index.js
@@ -9,92 +9,64 @@ const { getMainDefinition } = require("apollo-utilities");
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-/**
- * 
- * @param {{ httpEndpoint: string, wsEndpoint: string, bearerToken: string }} params 
- * @returns { ApolloClient }
- */
+const GRAPHQL_ENDPOINT = "https://api.ecomon.no";
+const GRAPHQL_ENDPOINT_WS = "wss://api.ecomon.no";
+const TOKEN = "intermediary-api-key_858bef2f-5b67-49b2-b7e0-c07a093f98be";
 
-const configureClient = ({ httpEndpoint, wsEndpoint, bearerToken }) => {
-  const wsLink = new WebSocketLink({
-    uri: wsEndpoint,
-    options: {
-      reconnect: true,
-      connectionParams: async () => {
-        return {
-          Authorization: bearerToken
-        };
+const wsLink = new WebSocketLink({
+  uri: GRAPHQL_ENDPOINT_WS,
+  options: {
+    reconnect: true,
+    connectionParams: async () => {
+      return {
+        Authorization: `Bearer ${TOKEN}`
+      };
+    }
+  },
+  webSocketImpl: WebSocket
+});
+
+const httpLink = createHttpLink({
+  uri: GRAPHQL_ENDPOINT,
+  fetch: fetch,
+  headers: {
+    Authorization: `Bearer ${TOKEN}`
+  }
+});
+
+const splitLink = ApolloLink.split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const apolloClient = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: splitLink
+});
+
+// Test subscription
+apolloClient
+  .subscribe({
+    query: gql`
+      subscription {
+        realtimeMeasurement(deviceId: "pM8SEyRQ") {
+          activePowerImport
+          time
+        }
       }
-    },
-    webSocketImpl: WebSocket
-  });
-
-  const httpLink = createHttpLink({
-    uri: httpEndpoint,
-    fetch: fetch,
-    headers: {
-      Authorization: bearerToken
+    `,
+    variables: {}
+  })
+  .subscribe({
+    next(data) {
+      console.log(data);
     }
   });
-
-  const splitLink = ApolloLink.split(
-    ({ query }) => {
-      const definition = getMainDefinition(query);
-
-      return (
-        definition.kind === "OperationDefinition" &&
-        definition.operation === "subscription"
-      );
-    },
-    wsLink,
-    httpLink
-  );
-  const apolloClient = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: splitLink
-  });
-
-  return apolloClient
-}
-
-
-
-/**
- * Example
- * const GRAPHQL_ENDPOINT = "https://api.ecomon.no";
- * const GRAPHQL_ENDPOINT_WS = "wss://api.ecomon.no";
- * const TOKEN = "Bearer intermediary-api-key_858bef2f-5b67-49b2-b7e0-c07a093f98be";
- * 
- * const client = configureClient({
- *  httpEndpoint: GRAPHQL_ENDPOINT,
- *  wsEndpoint: GRAPHQL_ENDPOINT_WS,
- *  bearerToken: TOKEN
- * })
- * 
- * // Test subscription
- * client
- * .subscribe({
- *   query: gql`
- *     subscription {
- *       realtimeMeasurement(deviceId: "pM8SEyRQ") {
- *         activePowerImport
- *         time
- *       }
- *     }
- *   `,
- *   variables: {}
- * })
- * .subscribe({
- *   next(data) {
- *     console.log(data);
- *   }
- * });
- * 
- */
-
-
-
-
-
-
-module.exports = configureClient
